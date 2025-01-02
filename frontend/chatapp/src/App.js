9f78e5facc77228c40d2e2f10302bc7b2177af6b
“Aleemuddin <aleem2747@gmail.com>
2025-01-01 21:02:28 -0600

    connecting backend and frontend

--- frontend/chatapp/src/App.js
+++ frontend/chatapp/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { connect, sendMsg } from './api';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    connect();
+}
+
+send() {
+  console.log('Hellou');
+  sendMsg("Hellou");
+}
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={this.send} > Hit  </button>
     </div>
   );
 }
-
+}
 export default App;
